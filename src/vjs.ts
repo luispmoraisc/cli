b94ffb538436dd8b58b69a755dafc0d0cfe7de83
Lennon Vinicius Alves Dias <lennon.t1340.randstad@xpi.com.br>
2019-10-29 18:57:09 -0300

    refactor loader

--- src/vjs.ts
+++ src/vjs.ts
@@ -6,7 +6,7 @@ const minimist = require('minimist');
 const generateProject = require('./project/generate');
 const generateComponent = require('./component/generate');
 const { exec } = require('child_process');
-const { Spinner } = require('cli-spinner');
+const ora = require('ora');
 
 module.exports = async (args: { slice: (arg0: number) => void; }) => {
     const argv = minimist(args.slice(2), { '--': true });
@@ -19,12 +19,12 @@ module.exports = async (args: { slice: (arg0: number) => void; }) => {
     if (projectName) {
         await generateProject(projectName, authorName, description);
 
-        const spinner = new Spinner('Installing dependencies');
-        spinner.start();
+        const spinner = ora(`Creating project and installing dependencies`).start();
         
         await exec(`cd ${projectName} && git init && npm install`, function (error: Error, stdout: any, stderr: any) {
-            // if (error || stderr) console.log(error || stderr);
             spinner.stop();
+            spinner.clear();
+            if (error || stderr) console.log(error || stderr);
         });
     }
 
